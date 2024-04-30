81dcc7a0234c405a1f89d15d392444df99e182a8
AkramjonDev <akramjon.ikromov0523@gmail.com>
2024-04-30 23:18:12 +0500

    some changes in useFetch

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,25 +1,18 @@
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
-import { useState } from "react";
+import React from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from "react-native";
 import { COLORS, SIZES } from "../../../constants";
 import styles from "./popularjobs.style";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 import { useRouter } from "expo-router";
-import useFetch from '../../../hook/useFetch'
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const {data, isLoading, error} = useFetch
-  ('search', {
+  const { data, isLoading, error } = useFetch('search', {
     query: 'AkramjonDev',
     num_page: 1
-  })
-  console.log(data);
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -32,11 +25,11 @@ const Popularjobs = () => {
         {isLoading ? (
           <ActivityIndicator size={"large"} color={COLORS.primary} />
         ) : error ? (
-          <Text>ERROR</Text>
+          <Text>Error: {error.message}</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5]}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />} 
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal

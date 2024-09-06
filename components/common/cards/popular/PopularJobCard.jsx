7d089ce0d115733029db6a4f180a5fafeff40886
AkramjonDev <akramjon.ikromov0523@gmail.com>
2024-09-06 17:48:53 +0500

    some changes are made

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -4,6 +4,8 @@ import styles from "./popularjobcard.style";
 import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  if (!item) return null; // Prevent rendering if item is null or undefined
+
   return (
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
@@ -16,7 +18,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
               ? item.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
-          resizeMode='contain'
+          resizeMode="contain"
           style={styles.logoImage}
         />
       </TouchableOpacity>
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Key": "94d2070e7fmsheb557827f34f889p1080fdjsnd3553dd35dc9",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
@@ -26,7 +26,7 @@ const useFetch = (endpoint, query) => {
       setIsLoading(false);
     } catch (error) {
       setError(error);
-      console.log(error)
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
